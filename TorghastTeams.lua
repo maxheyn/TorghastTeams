bc9f4b37aea0a1b8ee72195b42a90b7b471d06d6
Max Heyn <maxoheyn@gmail.com>
2020-12-18 19:42:42 -0500

    initial frame

--- TorghastTeams.lua
+++ TorghastTeams.lua
@@ -1,4 +1,13 @@
-local EventFrame = CreateFrame("frame", "EventFrame")
+
+-- User Configuration Frame
+local UIConfig = CreateFrame("Frame", "TorghastTeams_ConfigFrame", UIParent, "BasicFrameTemplateWithInset")
+UIConfig:SetSize(280, 560)
+UIConfig:SetPoint("RIGHT", UIParent, "RIGHT")
+
+
+-----------------------------------------------------------------------------
+-- Event Listener Frame
+local EventFrame = CreateFrame("Frame", "EventFrame")
 EventFrame:RegisterEvent("BAG_UPDATE")
 
 EventFrame:SetScript("OnEvent", function(self, event, ...)
